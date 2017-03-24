184e6921314cfddef79e162e42bbdbcbcc446a2e
Jordan Burk <jordan.g.burk@gmail.com>
2017-03-24 00:56:45 -0700

    mongo basics

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017/tales_light';
+
+MongoClient.connect(url, (err, db) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.close();
+});
+
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.render(__dirname + "/index.pug");
 }).listen(3000);
